4f0c70b240857a5e57948777a6cc4386e01d1023
Dev-Joshua <Joshua_dup@hotmail.com>
2022-11-08 18:33:24 -0500

    creo componente para el navbar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import Login from "./components/login/login";
+import Navbar from "./components/navbar/navbar";
 
 //<Login /> para llamar los componentes
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Container>
         <Login />
       </Container>
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Form, Button } from "react-bootstrap";
+import "./login.css";
 
 export default class login extends React.Component {
   constructor(props) {
@@ -16,38 +17,23 @@ export default class login extends React.Component {
 
   render() {
     return (
-      <Container
-        id="login-container"
-        style={{
-          marginTop: 200,
-          width: "400px",
-          border: "1px solid #000",
-          padding: "30px",
-          borderRadius: 10,
-        }}
-      >
+      <Container id="login-container">
         <Form>
           <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label style={{ float: "left", fontWeight: "bold" }}>
-              Email
-            </Form.Label>
+            <Form.Label className="label">Email</Form.Label>
             <Form.Control
               //Proceso para capturar el dato(usuario)
               onChange={(e) => this.setState({ usuario: e.target.value })}
               placeholder="User"
-              style={{ fontSize: "12px" }}
             />
             {/* {this.state.usuario} */}
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label style={{ float: "left", fontWeight: "bold" }}>
-              Password
-            </Form.Label>
+            <Form.Label>Password</Form.Label>
             <Form.Control
               type="password"
               placeholder="Password"
-              style={{ fontSize: "12px" }}
               onChange={(e) => this.setState({ pass: e.target.value })}
             />
             {/* {this.state.pass} */}
@@ -55,7 +41,6 @@ export default class login extends React.Component {
           <Button
             variant="success"
             type="submit"
-            style={{ marginTop: 20, width: "100%" }}
             //Cuando le de click haga algo con ese evento
             onClick={() => {
               this.iniciarSesion();
