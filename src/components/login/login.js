528f36881b24ce489f5484a6f33bef848cbc84cc
Dev-Joshua <Joshua_dup@hotmail.com>
2022-11-09 00:24:28 -0500

    agrego ventana usuario en navbar

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Form, Button } from "react-bootstrap";
+import { Container, Form, Button, Row, Col } from "react-bootstrap";
 import "./login.css";
 
 export default class login extends React.Component {
@@ -18,37 +18,46 @@ export default class login extends React.Component {
   render() {
     return (
       <Container id="login-container">
-        <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label className="label">Email</Form.Label>
-            <Form.Control
-              //Proceso para capturar el dato(usuario)
-              onChange={(e) => this.setState({ usuario: e.target.value })}
-              placeholder="User"
-            />
-            {/* {this.state.usuario} */}
-          </Form.Group>
+        <Row>
+          <Col>
+            <Row>
+              <h2>Iniciar Sesión</h2>
+            </Row>
+            <Row>
+              <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label className="label">Email</Form.Label>
+                  <Form.Control
+                    //Proceso para capturar el dato(usuario)
+                    onChange={(e) => this.setState({ usuario: e.target.value })}
+                    placeholder="User"
+                  />
+                  {/* {this.state.usuario} */}
+                </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              onChange={(e) => this.setState({ pass: e.target.value })}
-            />
-            {/* {this.state.pass} */}
-          </Form.Group>
-          <Button
-            variant="success"
-            type="submit"
-            //Cuando le de click haga algo con ese evento
-            onClick={() => {
-              this.iniciarSesion();
-            }}
-          >
-            Iniciar Sesión
-          </Button>
-        </Form>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    onChange={(e) => this.setState({ pass: e.target.value })}
+                  />
+                  {/* {this.state.pass} */}
+                </Form.Group>
+                <Button
+                  variant="success"
+                  type="submit"
+                  //Cuando le de click haga algo con ese evento
+                  onClick={() => {
+                    this.iniciarSesion();
+                  }}
+                >
+                  Iniciar Sesión
+                </Button>
+              </Form>
+            </Row>
+          </Col>
+        </Row>
       </Container>
     );
   }
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,6 +1,16 @@
 import React from "react";
-import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
+import {
+  Container,
+  Navbar,
+  Nav,
+  DropdownButton,
+  Dropdown,
+  Row,
+  Col,
+} from "react-bootstrap";
 import "./navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
 
 export default class menu extends React.Component {
   constructor(props) {
@@ -10,28 +20,27 @@ export default class menu extends React.Component {
 
   render() {
     return (
-      <Navbar bg="dark" expand="lg">
+      <Navbar fixed="top" id="navbar" bg="dark" variant="dark" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">Tu-mascota</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
+              <Nav.Link href="#home">Inicio</Nav.Link>
+              <Nav.Link href="#link">Anuncios</Nav.Link>
             </Nav>
+            <DropdownButton id="dropdown-basic-button" title="Usuario">
+              <Dropdown.Header>
+                <Row>
+                  <FontAwesomeIcon icon={faUserSecret} />
+                </Row>
+                <Row>#USUARIO#</Row>
+              </Dropdown.Header>
+              <Dropdown.Divider />
+              <Dropdown.Item href="#/action-1">Cerrar Sesión</Dropdown.Item>
+              {/* <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item> */}
+            </DropdownButton>
           </Navbar.Collapse>
         </Container>
       </Navbar>
